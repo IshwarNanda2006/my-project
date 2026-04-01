43231b2bd5ac375e63069196b9a95b534bcaf6a9
Ishwar_Nanda <nandaishwar30@gmail.com>
2026-04-02 00:05:03 +0530

    UC3 ;display hello with name or default world

---
+++ HelloWorld3.java
@@ -0,0 +1,14 @@
+public class HelloWorld3 {
+    public static void main(String[] args) {
+
+        String name;
+
+        if (args.length == 0) {
+            name = "World";
+        } else {
+            name = args[0];
+        }
+
+        System.out.println("Hello " + name);
+    }
+}
